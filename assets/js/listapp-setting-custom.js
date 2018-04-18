944b39070e9aad7350353472c8572cbc283b9f10
tony <thang@beostore.io>
2018-04-18 09:34:52 +0700

    update refactor

--- assets/js/listapp-setting-custom.js
+++ assets/js/listapp-setting-custom.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function(){
 	 jQuery("button.json-editor-btn-add").siblings(".json-editor-btn-delete").hide();
 	 jQuery("button.json-editor-btn-add").text("Add new Row");
-	
 });
